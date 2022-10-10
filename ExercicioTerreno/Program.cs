0f12298238edbadf89f160d66123e80a571d2a9a
Gabriel Watanabe Pereira <102392345+gabrielwatanabe043@users.noreply.github.com>
2022-10-10 14:01:26 -0300

    Add files via upload

---
+++ ExercicioTerreno/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace ExercicioTerreno
+{
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+            double area,preco;
+            Console.WriteLine("Digite largura do terreno: ");
+            double largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite comprimento do terreno:");
+            double comprimento = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+
+            Console.WriteLine("Digite valor do M2");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            preco = valor * area;
+
+            Console.WriteLine("AREA: "+ area.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("PRECO: " + preco.ToString("F2", CultureInfo.InvariantCulture));
+        }
+}
+}
